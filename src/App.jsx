daac6139b37150827e24d4de84697db01c9eb8da
GUIFRE88 <guilherme.fre073@gmail.com>
2025-02-11 07:25:37 -0300

    Add search bar and filter components to main layout
    - Implemented BarraDePesquisa component with search input
    - Created Filtro component for filtering content
    - Updated App.jsx and App.css to include new components in a responsive layout
    - Added search icon and styling for search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import BarraDePesquisa from './components/BarraDePesquisa'
+import Filtro from './components/Filtro'
 import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <>
+    <div className="container">
       <Sidebar />
-    </>
+      <div>
+        <BarraDePesquisa />
+        <Filtro />
+      </div>
+    </div>
   )
 }
 
